b5a684a360443daafae51975630077f89a632f5f
Rodriques <rodriques.perry@gmail.com>
2021-01-15 17:06:17 -0600

    Add doughnut graph and task table

--- js/index.js
+++ js/index.js
@@ -8,4 +8,31 @@ document.querySelector('.sideMenuToggler').addEventListener('click', function() 
   }
 });
 
-
+let doughnut = document.getElementById('projectStatusChart').getContext('2d');
+let doughnutChart = new Chart(doughnut, {
+  type: 'doughnut',
+  data: {
+    labels: ['Current', 'Overdue', 'Complete'],
+    datasets: [{
+      data: ['89', '33', '15'],
+      backgroundColor: [
+          'rgba(0, 255,0,.5)',
+          'rgba(255,0, 0, 0.5)',
+          'rgba(255, 206, 86, 0.5)'
+      ],
+      borderColor: [
+          'rgba(0, 255, 0, 1)',
+          'rgba(255,0, 0, 1)',
+          'rgba(255, 206, 86, 1)'
+      ],
+      borderWidth: 1
+     
+  }]
+},
+options: {
+  cutoutPercentage: 85,
+  legend: {
+    display: false
+  }
+}
+});
