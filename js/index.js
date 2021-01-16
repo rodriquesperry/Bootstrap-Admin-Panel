664b4c3aa57a1c163473ac738a8bf43610ef5429
Rodriques <rodriques.perry@gmail.com>
2021-01-16 16:29:13 -0600

    Add Javascript calendar

--- js/index.js
+++ js/index.js
@@ -36,3 +36,52 @@ options: {
   }
 }
 });
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const today = new Date();
+const currMonth = today.getMonth();
+const currYear = today.getFullYear();
+const monthName = months[currMonth];
+
+showCalendar(currMonth, currYear);
+
+const monthAndYear = document.querySelector('#monthAndYear');
+monthAndYear.innerHTML = `${monthName} ${currYear}`;
+
+function showCalendar(month, year) {
+  let fisrtDay = (new Date(year, month)).getDay();
+  
+  tbody = document.querySelector('#calBody');
+  // tbody.innerHTML = '';
+
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement('tr');
+
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < fisrtDay) {
+        let cell = document.createElement('td');
+        cellText = document.createTextNode('');
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+          cell = document.createElement('td');
+          cellText = document.createTextNode(date);
+          if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+            cell.classList.add('bg-danger');
+          }
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+          date++;
+      }
+    }
+    tbody.appendChild(row);
+  }
+
+  function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+  }
+
+}
